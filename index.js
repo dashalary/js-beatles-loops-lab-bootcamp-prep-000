0e36b6c14edf06b5d459fdf07eb3c9636bc8c51f
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-16 19:15:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay() {
-  var array = ['John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums'];
-  for (let i=0; )
+  var array = [`"John Lennon plays guiltar"`];
+  for (let i = 0; i<4; i++) {
+    
+  }
 }
   return array;
 }
