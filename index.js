7f9fdbab957c5cf558d98c2501d7a6d967316813
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-16 19:49:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function theBeatlesPlay() {
   var array = [];
-  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+ 
   for (let i = 0; i<4; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
