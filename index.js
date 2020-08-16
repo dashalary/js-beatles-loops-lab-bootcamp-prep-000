c57eaf4432e78ccc8a0c2c0d23e6c8c8ce277d23
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-16 19:59:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var array = []
 
-function theBeatlesPlay() {
+function theBeatlesPlay(musicians, instruments) {
 
   for (let i = 0; i<4; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
